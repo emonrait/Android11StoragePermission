697758ff529b5e737001661c3781ff20c263b00e
Raihan Mahamud <emonrait@gmail.com>
2021-11-21 14:05:25 +0600

    Permission Update

--- app/src/main/java/com/example/androidstorage11permission/MainActivity.kt
+++ app/src/main/java/com/example/androidstorage11permission/MainActivity.kt
@@ -19,12 +19,18 @@ import android.os.Build.VERSION
 import android.os.Build.VERSION.SDK_INT
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
+import android.app.Activity
 
 import androidx.core.app.ActivityCompat
 
 import androidx.core.app.ActivityCompat.startActivityForResult
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
+import android.graphics.pdf.PdfDocument
 import android.net.Uri
 
 import android.os.Build.VERSION.SDK_INT
@@ -32,24 +38,61 @@ import android.provider.Settings
 import android.view.MenuItem
 import java.lang.Exception
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.Nullable
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
+    var file_name_path = ""
     private val PERMISSION_REQUEST_CODE = 2296
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    var permission =
+        arrayOf<String>(
+            READ_EXTERNAL_STORAGE,
+            WRITE_EXTERNAL_STORAGE
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (checkPermission()) {
-            Toast.makeText(
-                this,
-                "Download File",
-                Toast.LENGTH_SHORT
-            )
-                .show()
+            directoryCreate()
+            createpdf()
         } else {
             requestPermission()
         }
+
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    if (SDK_INT >= Build.VERSION_CODES.R) {
+                        if (Environment.isExternalStorageManager()) {
+                            // perform action when allow permission success
+                            Toast.makeText(
+                                this,
+                                "Permission Granted",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Permission Denied",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                    }
+                }
+
+            }
 
     }
 
@@ -71,11 +114,13 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
                 intent.addCategory("android.intent.category.DEFAULT")
                 intent.data = Uri.parse(String.format("package:%s", applicationContext.packageName))
-                startActivityForResult(intent, 2296)
+                //startActivityForResult(intent, 2296)
+                activityResultLauncher.launch(intent)
             } catch (e: Exception) {
                 val intent = Intent()
                 intent.action = Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
-                startActivityForResult(intent, 2296)
+                //startActivityForResult(intent, 2296)
+                activityResultLauncher.launch(intent)
             }
         } else {
             //below android 11
@@ -96,9 +141,11 @@ class MainActivity : AppCompatActivity() {
 
 
                 } else {
-                    Toast.makeText(this,
+                    Toast.makeText(
+                        this,
                         "Allow permission for storage access!",
-                        Toast.LENGTH_SHORT)
+                        Toast.LENGTH_SHORT
+                    )
                         .show()
                 }
             }
@@ -137,4 +184,191 @@ class MainActivity : AppCompatActivity() {
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+
+    fun createpdf() {
+
+        var bankasia_name = "Bank Asia Limited";
+        var statementTitle =
+            "Account Statement" + "(" + "etFromDate.text.toString()" + " to " + "etToDate.text.toString()" + ")"
+
+        val bounds = Rect()
+        val pageWidth = 700
+        val pageheight = 470
+        val pathHeight = 2
+        val fileName = "DateUtil.getVisaCurDateTime()"
+        // file_name_path = "/visacard_receit/$fileName.pdf"
+        file_name_path =
+            "/" + "TextContants.smartapp_document_folder" + "/" + "account statement" + fileName + ".pdf"
+        val myPdfDocument = PdfDocument()
+        val paint = Paint()
+        val paint2 = Paint()
+        val path = Path()
+        val myPageInfo =
+            PdfDocument.PageInfo.Builder(pageWidth, pageheight, 1).create()
+        val documentPage =
+            myPdfDocument.startPage(myPageInfo)
+        val canvas = documentPage.canvas
+        var y = 25 // x = 10,
+        //int x = (canvas.getWidth() / 2)
+        var x = 10
+        paint.getTextBounds(
+            bankasia_name,
+            0,
+            bankasia_name.length,
+            bounds
+        )
+        x = canvas.width / 2 - bounds.width() / 2
+        canvas.drawText(bankasia_name, x.toFloat(), y.toFloat(), paint)
+
+        paint.getTextBounds(
+            statementTitle,
+            0,
+            statementTitle.length,
+            bounds
+        )
+
+        x = canvas.width / 2 - bounds.width() / 2
+
+        y += paint.descent().toInt() - paint.ascent().toInt()
+        //(y += paint.descent() - paint.ascent())
+        canvas.drawText(statementTitle, x.toFloat(), y.toFloat(), paint)
+        y += paint.descent().toInt() - paint.ascent().toInt()
+        canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+
+        //horizontal line
+        path.lineTo(pageWidth.toFloat(), pathHeight.toFloat())
+        paint2.color = Color.GRAY
+        paint2.style = Paint.Style.STROKE
+        path.moveTo(x.toFloat(), y.toFloat())
+        canvas.drawLine(0f, y.toFloat(), pageWidth.toFloat(), y.toFloat(), paint2)
+
+        //blank space
+        y += paint.descent().toInt() - paint.ascent().toInt()
+        canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+        y += paint.descent().toInt() - paint.ascent().toInt()
+        x = 10
+        canvas.drawText("SL", x.toFloat(), y.toFloat(), paint)
+        canvas.drawText("Date", 70F, y.toFloat(), paint)
+        canvas.drawText("Debit Amount", 170F, y.toFloat(), paint)
+        canvas.drawText("Credit Amount", 270F, y.toFloat(), paint)
+        canvas.drawText("Balance", 370F, y.toFloat(), paint)
+        canvas.drawText("Particurlars", 470F, y.toFloat(), paint)
+        y += paint.descent().toInt() - paint.ascent().toInt().toInt()
+        x = 10
+        canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+
+        //blank space
+        y += paint.descent().toInt() - paint.ascent().toInt()
+        canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+
+        //horizontal line
+        path.lineTo(pageWidth.toFloat(), pathHeight.toFloat())
+        paint2.color = Color.GRAY
+        paint2.style = Paint.Style.STROKE
+        path.moveTo(x.toFloat(), y.toFloat())
+        canvas.drawLine(0f, y.toFloat(), pageWidth.toFloat(), y.toFloat(), paint2)
+
+        /*   //  while (accountList)
+            for (i in 0 until accountList!!.size) {
+
+                y += paint.descent().toInt() - paint.ascent().toInt()
+                canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+                y += paint.descent().toInt() - paint.ascent().toInt()
+                canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+
+                x = 10
+                canvas.drawText(accountList[i].slNo.toString(), x.toFloat(), y.toFloat(), paint)
+                canvas.drawText(
+                    accountList[i].transactionDate.toString(),
+                    70F,
+                    y.toFloat(),
+                    paint
+                )
+                canvas.drawText(accountList[i].debtAmount.toString(), 170F, y.toFloat(), paint)
+                canvas.drawText(
+                    accountList[i].creditAmount.toString(),
+                    270F,
+                    y.toFloat(),
+                    paint
+                )
+                canvas.drawText(
+                    accountList[i].availaleBalance.toString(),
+                    370F,
+                    y.toFloat(),
+                    paint
+                )
+                canvas.drawText(accountList[i].remarks.toString(), 470F, y.toFloat(), paint)
+
+
+                y += paint.descent().toInt() - paint.ascent().toInt().toInt()
+                x = 10
+                canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+
+                //blank space
+                y += paint.descent().toInt() - paint.ascent().toInt()
+                canvas.drawText("", x.toFloat(), y.toFloat(), paint)
+                //horizontal line
+                path.lineTo(pageWidth.toFloat(), pathHeight.toFloat())
+                paint2.color = Color.GRAY
+                paint2.style = Paint.Style.STROKE
+                path.moveTo(x.toFloat(), y.toFloat())
+                canvas.drawLine(0f, y.toFloat(), pageWidth.toFloat(), y.toFloat(), paint2)
+            }
+    */
+
+        myPdfDocument.finishPage(documentPage)
+//        val file = File(
+//            Environment.getExternalStorageDirectory().toString() + file_name_path
+//        )
+
+        // val downloadFilePath =Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+
+        var rootPath = this.getExternalFilesDir(null)?.absolutePath;
+        try {
+            myPdfDocument.writeTo(FileOutputStream(rootPath + file_name_path))
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        myPdfDocument.close()
+
+        //  viewPdfFile()
+
+        // openStorageAccess()
+
+        // selectPdfFromStorage()
+
+
+    }
+
+    fun directoryCreate() {
+
+        try {
+//            val file = File(
+//                Environment.getExternalStorageDirectory(),
+//                TextContants.smartapp_document_folder
+//            )
+
+            /*val file = File(
+                this.getExternalFilesDir(null)?.absolutePath,
+                "StorageCheck"
+            )*/
+
+            val dir = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + File.separator
+                        + applicationContext.resources.getString(R.string.app_name)
+                        + File.separator
+            )
+
+            if (!dir.exists()) {
+                dir.mkdir()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+
 }
